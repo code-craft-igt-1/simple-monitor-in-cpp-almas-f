252735cda56a054b5a60381dfb17e52a3f8271a0
almas-f <62561466+almas-f@users.noreply.github.com>
2024-09-04 12:16:27 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -4,5 +4,42 @@
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
+}
+
+TEST(Monitor, OkWhenAnyVitalIsInRange) {
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+}
+
+TEST(Monitor, NotOkWhenTemperatureIsTooHigh) {
+  ASSERT_FALSE(vitalsOk(102.1, 75, 95));
+}
+
+TEST(Monitor, NotOkWhenTemperatureIsTooLow) {
+  ASSERT_FALSE(vitalsOk(94.9, 75, 95));
+}
+
+TEST(Monitor, OkWhenTemperatureIsInRange) {
+  ASSERT_TRUE(vitalsOk(98.6, 75, 95));
+}
+
+// Test pulse rate boundary conditions
+TEST(Monitor, NotOkWhenPulseRateIsTooHigh) {
+  ASSERT_FALSE(vitalsOk(98.6, 101, 95));
+}
+
+TEST(Monitor, NotOkWhenPulseRateIsTooLow) {
+  ASSERT_FALSE(vitalsOk(98.6, 59, 95));
+}
+
+TEST(Monitor, OkWhenPulseRateIsInRange) {
+  ASSERT_TRUE(vitalsOk(98.6, 75, 95));
+}
+
+// Test SPO2 boundary conditions
+TEST(Monitor, NotOkWhenSpo2IsTooLow) {
+  ASSERT_FALSE(vitalsOk(98.6, 75, 89));
+}
+
+TEST(Monitor, OkWhenSpo2IsInRange) {
+  ASSERT_TRUE(vitalsOk(98.6, 75, 95));
 }
